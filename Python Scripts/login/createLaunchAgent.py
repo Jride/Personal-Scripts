4c4050d0df82cb4a75d6dbd93a2a1e53bc61c7c1
Josh Rideout <joshrideout88@gmail.com>
2019-02-25 10:23:39 +0000

    updated loginhook script

--- Python Scripts/login/createLaunchAgent.py
+++ Python Scripts/login/createLaunchAgent.py
@@ -37,7 +37,7 @@ file.write('''
     <key>ProgramArguments</key>
     <array>
         <string>/usr/local/bin/python3</string>
-        <string>/Users/joshrideout/Documents/.packageUpdates/loginHookAlias.sh</string>
+        <string>/Users/joshrideout/Documents/.packageUpdates/loginHookAlias.py</string>
     </array>
     <key>RunAtLoad</key>
     <true/>
