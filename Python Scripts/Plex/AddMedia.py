ef0474d98f7094202746c3988ba25cd379b8401a
Josh Rideout <joshrideout88@gmail.com>
2021-06-16 22:24:12 +0100

    update add media

--- Python Scripts/Plex/AddMedia.py
+++ Python Scripts/Plex/AddMedia.py
@@ -16,22 +16,32 @@ parser = itv_argparser.parser(
     Start download a torrent via a magnet url
     '''
 )
+parser.add_argument('--show', help='Adds tv show', action="store_true")
+parser.add_argument('--movie', help='Adds move', action="store_true")
+parser.add_argument('--magnet', help='magnet link')
 args = parser.parse_args(sys.argv[1:])
 
 def run(command):
     itv_shell.run("ssh -t plex@plex-server.myddns.me -p 8888 \"transmission-remote --auth transmission:transmission %s\"" % (command))
 
-media_types = {
-    "TV Show": "tv_shows",
-    "Movie": "movies"
-}
-media_options = list(media_types.keys())
-selected_index = itv_shell.choose_from_list("What type of media?", media_options)
-selected_media = media_options[selected_index]
-folder_name = media_types[selected_media]
+if args.show:
+    folder_name = "tv_show"
+elif args.movie:
+    folder_name = "movies"
+else:
+    media_types = {
+        "TV Show": "tv_shows",
+        "Movie": "movies"
+    }
+    media_options = list(media_types.keys())
+    selected_index = itv_shell.choose_from_list("What type of media?", media_options)
+    selected_media = media_options[selected_index]
+    folder_name = media_types[selected_media]
 
-input("\nCopy magenet URL to clipboard and press enter")
-
-magnet_link = pc.paste()
+if args.magnet:
+    magnet_link = args.magnet
+else:
+    input("\nCopy magenet URL to clipboard and press enter")
+    magnet_link = pc.paste()
 
 run("-w ~/torrents/%s/ -a '%s'" % (folder_name, magnet_link))
